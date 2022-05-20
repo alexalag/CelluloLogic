f85c7ed996a42f81143dd597ad780a98b65edcfe
fannylsn <91012518+fannylsn@users.noreply.github.com>
2022-05-20 18:16:07 +0200

    avancée des settings

--- CelluloLogicGame/Assets/Scripts/Menu/MainMenu.cs
+++ CelluloLogicGame/Assets/Scripts/Menu/MainMenu.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
     public Animator animator;
     public AudioSource audioSource;
     public AudioClip audioClip;
+    public Toggle arrows1;
+    public Toggle wasd1;
+    public Toggle arrows2;
+    public Toggle wasd2;
     private bool gameQuit;
 
     void Start()
@@ -38,6 +42,42 @@ public class MainMenu : MonoBehaviour
             audioSource.Play();
             animator.SetTrigger("Clignoti");
             Invoke("Flickering", 10f);
+        }
+    }
+
+    public void ArrowsInputPlayer1(bool isOn) {
+        if(isOn) {
+            ConstantsGame.trueInput = InputKeyboard.arrows;
+            if(arrows2.isOn) arrows2.isOn = false;
+            if(wasd1.isOn) wasd1.isOn = false;
+            if(!wasd2.isOn) wasd2.isOn = true;  
+        }
+    }
+
+    public void WASDInputPlayer1(bool isOn) {
+        if(isOn) {
+            ConstantsGame.trueInput = InputKeyboard.wasd;
+            if(!arrows2.isOn) arrows2.isOn = true;
+            if(arrows1.isOn) arrows1.isOn = false;
+            if(wasd2.isOn) wasd2.isOn = false; 
+        }
+    }
+
+    public void ArrowsInputPlayer2(bool isOn) {
+        if(isOn) {
+            ConstantsGame.falseInput = InputKeyboard.arrows;
+            if(arrows1.isOn) arrows1.isOn = false;
+            if(wasd2.isOn) wasd2.isOn = false;
+            if(!wasd1.isOn) wasd1.isOn = true; 
+        }
+    }
+
+    public void WASDInputPlayer2(bool isOn) {
+        if(isOn) {
+            ConstantsGame.falseInput = InputKeyboard.wasd;
+            if(!arrows1.isOn) arrows1.isOn = true;
+            if(arrows2.isOn) arrows2.isOn = false;
+            if(wasd1.isOn) wasd1.isOn = false;
         }
     }
 }
