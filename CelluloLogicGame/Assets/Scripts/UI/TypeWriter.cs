a15bcbb082ebc5295300dca4e2c3427a649a131e
tamaraaPrs <100276092+tamaraaPrs@users.noreply.github.com>
2022-05-28 16:17:58 +0200

    ajout des boites de dialogue

--- CelluloLogicGame/Assets/Scripts/UI/TypeWriter.cs
+++ CelluloLogicGame/Assets/Scripts/UI/TypeWriter.cs
@@ -18,12 +18,12 @@ public class TypeWriter : MonoBehaviour
 
     // Affiche les lettres du texte une à une avec le son du typing
     IEnumerator ShowText() {
-        for(int i = 0; i < fullText.Length; ++i) {
+        for(int i = 1; i <= fullText.Length; ++i) {
             if(!audioSource.isPlaying) {
                 audioSource.Play();
             }
             if(!ConstantsUI.writing) {
-                i = fullText.Length -1;
+                i = fullText.Length;
             }
             currentText = fullText.Substring(0,i);
             GetComponent<TextMeshProUGUI>().text = currentText;
