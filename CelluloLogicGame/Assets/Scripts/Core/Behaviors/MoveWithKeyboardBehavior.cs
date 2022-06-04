7b830eecf99d490a554e29b23eb91b7f0aa11646
alexalag <alexandra.lagutova@gmail.com>
2022-06-04 15:11:25 +0200

    normalement les colisions doivent marcher

--- CelluloLogicGame/Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ CelluloLogicGame/Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -15,6 +15,7 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
 
     private bool onStone;
     private bool collisionBehavior;
+    Steering steering = new Steering();
 
     private GameManager gameManager;
 
@@ -53,39 +54,41 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     }
 
     public override Steering GetSteering(){
-        if(!ConstantsGame.gameIsRunning) return new Steering();
-        InputKeyboard inputKeyboard = CelluloName == "True" ? ConstantsGame.trueInput : ConstantsGame.falseInput;
-        float xAxis;
-        float zAxis;
 
-        if( inputKeyboard == 0 ) {
-            xAxis = Input.GetAxis ("Horizontal") ;
-            zAxis = Input.GetAxis ("Vertical") ;
-        } else {
-            xAxis = Input.GetAxis("HorizontalWASD") ;
-            zAxis = Input.GetAxis ("VerticalWASD") ;
-        }
+            if(!ConstantsGame.gameIsRunning) return new Steering();
+            InputKeyboard inputKeyboard = CelluloName == "True" ? ConstantsGame.trueInput : ConstantsGame.falseInput;
+            float xAxis;
+            float zAxis;
+
+            if( inputKeyboard == 0 ) {
+                xAxis = Input.GetAxis ("Horizontal") ;
+                zAxis = Input.GetAxis ("Vertical") ;
+            } else {
+                xAxis = Input.GetAxis("HorizontalWASD") ;
+                zAxis = Input.GetAxis ("VerticalWASD") ;
+            }
         
-        Steering steering = new Steering();
-        if(onStone) {
-            steering.linear = new Vector3(xAxis, 7, zAxis)*agent.maxAccel;
-        } else {
-            steering.linear = new Vector3(xAxis, 0, zAxis)*agent.maxAccel;
-        }
+            Steering steering = new Steering();
+            if(onStone) {
+                steering.linear = new Vector3(xAxis, 7, zAxis)*agent.maxAccel;
+            } else {
+                steering.linear = new Vector3(xAxis, 0, zAxis)*agent.maxAccel;
+            }
 
-        Vector3 direction = new Vector3(xAxis, 0, zAxis);
-        if(unactiveCellulo.type == 0 && unactiveCellulo.getIsDrawed() && unactiveCellulo.IsPlayerThatDraw(this.gameObject))
-        {
-            direction = unactiveCellulo.gameObject.transform.position - transform.position;
-        } 
-        if (direction != Vector3.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z), Vector3.up);
-            transform.rotation =
-                Quaternion.RotateTowards(transform.rotation, targetRotation, 700 * Time.deltaTime);
-        }
-
-        steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear , agent.maxAccel));
+            Vector3 direction = new Vector3(xAxis, 0, zAxis);
+            if(unactiveCellulo.type == 0 && unactiveCellulo.getIsDrawed() && unactiveCellulo.IsPlayerThatDraw(this.gameObject))
+            {
+                direction = unactiveCellulo.gameObject.transform.position - transform.position;
+            } 
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z), Vector3.up);
+                transform.rotation =
+                    Quaternion.RotateTowards(transform.rotation, targetRotation, 700 * Time.deltaTime);
+            }
+            
+            steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear , agent.maxAccel));
+        
         return steering;
     }
 
